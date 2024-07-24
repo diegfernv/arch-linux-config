acbf9f7497dd752bccac9497aa01a36309867c2e
Raniwis <rosemary.9816@hotmail.com>
2024-07-23 22:45:49 -0300

    Removed unnecessary files

--- dotfiles/polybar/.config/polybar/custom.old/custom-scripts/toggle_keyboard_layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
-
-if [ $current_layout == "us" ]; then
-    setxkbmap -layout es
-else
-    setxkbmap -layout us
-fi
